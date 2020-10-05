5141da4ca5c68daed7d90802c2e72f5d6c57b9ee
root <root@LAPTOP-RR93DGL7.localdomain>
2020-10-05 16:21:11 -0500

    Updates

---
+++ precache-manifest.d441c90714fbe8000e956e7f467f76d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12d8fe452c5aead3ef8e18317527b30c",
+    "url": "/dnd-website/index.html"
+  },
+  {
+    "revision": "1d52b5a67e8ba6f360d9",
+    "url": "/dnd-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea5cfc3202a04704897a",
+    "url": "/dnd-website/static/js/2.a51ad747.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dnd-website/static/js/2.a51ad747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d52b5a67e8ba6f360d9",
+    "url": "/dnd-website/static/js/main.3cf057fe.chunk.js"
+  },
+  {
+    "revision": "f6c679313ab92c7f1102",
+    "url": "/dnd-website/static/js/runtime-main.c39b6b3a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dnd-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
